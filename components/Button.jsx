8a569bb26e19d4ba04b97191c77a8821c12edd53
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-11-07 17:47:02 -0700

    finish setting up the test environment

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,10 @@
-function Button({ Sign }) {
+function Button({ Sign, onClick }) {
   return (
     <div className="flex p-4">
-      <button className="flex justify-center items-center py-4 px-8 rounded-lg cursor-pointer bg-[#00FF87] text-black">
+      <button
+        className="flex justify-center items-center py-4 px-8 rounded-lg cursor-pointer bg-[#00FF87] text-black"
+        onClick={onClick}
+      >
         {Sign}
       </button>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,69 @@ import { useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  const [add, setAdd] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+    if (count > 100) {
+      setCount(0);
+    }
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+    if (count < -100) {
+      setCount(0);
+    }
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  const handleInput = (e) => {
+    setCount(parseInt(count) + parseInt(add));
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setAdd(e.target.value);
+  };
   return (
-    <div className="flex flex-col items-center justify-start w-full h-screen">
-      <div className="flex flex-row">
-        <h1>Counter: {count}</h1>
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <div className="flex flex-col items-center justify-center p-24 rounded-xl border-4 border-[#00ff87]">
+        <div className="flex flex-row mb-8">
+          <h1>Counter: </h1>
+          <h1 className="ml-2">{count}</h1>
+        </div>
+
+        <div className="flex flex-row">
+          <Button Sign="+" onClick={increment} />
+          <Button Sign="-" onClick={decrement} />
+          <Button Sign="Reset" onClick={reset} />
+        </div>
+
+        <div className="flex flex-row">
+          <form onSubmit={handleInput}>
+            <div className="flex flex-row justify-center items-center">
+              <input
+                type="number"
+                name="add"
+                id="number"
+                onChange={handleChange}
+                className="px-8 py-4 border-[#00FF87] border-2 rounded-lg mr-4"
+                placeholder="Enter to add"
+              />
+
+              <button
+                type="submit"
+                className="flex justify-center items-center py-4 px-8 rounded-lg cursor-pointer bg-[#00FF87] text-black"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
